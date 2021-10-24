11bd61fca3ef56279dbbc7c2cca52b278da11b6f
Davis Tibbz <davisinyo@gmail.com>
2021-10-24 18:12:00 +0800

    Initial commit

---
+++ src/main/java/org/davistiba/App.java
@@ -0,0 +1,69 @@
+package org.davistiba;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author davis t.
+ * Simple Java server program, using built-in libraries.
+ * (except `org.json.JSONObject`)
+ * returns 200 OK JSON response with any REQUEST method.
+ */
+public class App {
+
+    static HttpServer server;
+
+    static ExecutorService executor = Executors.newFixedThreadPool(2);
+
+    public static void main(String[] args) throws IOException {
+
+        server = HttpServer.create(new InetSocketAddress(9999), 0);
+
+        server.createContext("/", new doHandle());
+        server.setExecutor(executor);
+        System.out.println("Listening on port " + server.getAddress());
+        server.start();
+
+    }
+
+    public static class doHandle implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+
+            InputStream in = exchange.getRequestBody();
+            OutputStream out = exchange.getResponseBody();
+            Headers headers = exchange.getResponseHeaders();
+
+            try (BufferedInputStream br = new BufferedInputStream(in)) {
+
+                //read the request
+                while (br.read() != -1) {
+                    //FIXME: always skips first character
+                    System.out.println(new String(br.readAllBytes()));
+                }
+
+                //send back response
+                final JSONObject baba = new JSONObject();
+                baba.put("message", "hello");
+                baba.put("datetime", LocalDateTime.now());
+
+                BufferedOutputStream bout = new BufferedOutputStream(out);
+
+                headers.set("Content-Type", "application/json");
+                exchange.sendResponseHeaders(200, baba.toString().length());
+                bout.write(baba.toString().getBytes());
+                bout.close();
+            }
+        }
+    }
+}
